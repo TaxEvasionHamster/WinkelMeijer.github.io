7bcff10da0f38e67592d2f79a286d35b98aebad5
TaxEvasionHamster <viggobleumink@icloud.com>
2025-03-02 17:36:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-const companyName = "JulianInc"; 
+const companyName = "Bouwbedrijf Winkelmeijer Junior"; 
 document.querySelectorAll('.companyName').forEach(element => {
   element.textContent = companyName;
 });
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementsByClassName("main-header").style.top = "0";
-  } else {
-    document.getElementsByClassName("main-header").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
